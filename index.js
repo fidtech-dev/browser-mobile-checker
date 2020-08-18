0607a70ba342e09e466781030b2754bc5014dbae
Alan <alan.derenzis@fidtech.net>
2020-08-18 14:21:07 -0300

    Added new features

--- index.js
+++ index.js
@@ -2,13 +2,13 @@
     "use strict";
     var that = {};
     that.version = "1.0.0";
-    that.supportsHtmlCapture = function() {
+    that.supportsHtmlCapture = () => {
         const i = document.createElement('input');
         i.setAttribute('capture',  'environment');
         return !!i['capture'];
     }
 
-    that.isMobileDevice = function() {
+    that.isMobileDevice = () => {
         if (isMobileBrowser()) {
             if (isArchFamilyArchitecture() || isIosArchitecture()) {
                 return true;
@@ -23,6 +23,62 @@
             return false;
         }
     }
+    that.isIAB = () => { // Is in app borwser
+        const rules = [
+            'WebView',
+            '(iPhone|iPod|iPad)(?!.*Safari\/)',
+            'Android.*(wv|\.0\.0\.0)',
+        ];
+        const regex = new RegExp(`(${rules.join('|')})`, 'ig');
+        return Boolean(navigator.userAgent.match(regex));
+    }
+    that.isFacebookIAB = () => {
+        return navigator.userAgent.match(/(FB_IAB)/i);
+    }
+    that.isInstagramIAB = () => {
+        return navigator.userAgent.match(/(Instagram)/i);
+    }
+    that.isAndroid  = () => {
+        return /Android/.test(navigator.userAgent);
+    }
+    that.isCordova  = () => {
+        return !!(window).cordova;
+    }
+    that.isEdge  = () => {
+        return /Edge/.test(navigator.userAgent);
+    }
+    that.isFirefox  = () => {
+        return /Firefox/.test(navigator.userAgent);
+    }
+    that.isChrome  = () => {
+        return  /Google Inc/.test(navigator.vendor);
+    }
+    that.isOriginalChrome  = () => {
+        return /Mobile Safari/.test(navigator.userAgent);
+    }
+    that.isChromeIOS  = () => {
+        return /CriOS/.test(navigator.userAgent);
+    }
+    that.isChromiumBased = () => {
+       return  !!(window).chrome && !/Edge/.test(navigator.userAgent)
+    }
+    that.isIE = () => {
+        return /Trident/.test(navigator.userAgent);
+    }
+    that.isIOS = () => {
+        return /(iPhone|iPad|iPod)/.test(navigator.platform);
+    }
+    that.isOpera = () => {
+        return /OPR/.test(navigator.userAgent);
+    }
+    that.isSafari = () => {
+        return /Safari/.test(navigator.userAgent) && !/Chrome/.test(navigator.userAgent);
+    }
+    that.isDuckDuckGo = () => {
+        return /DuckDuckGo/.test(navigator.userAgent);
+    }
+
+    // Internal functions
 
     function isArchFamilyArchitecture() {
         return navigator.platform.match(/arch/i);
@@ -49,4 +105,3 @@
         window.mobile_checker = that;
     }
 })();
-
